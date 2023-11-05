1edb4798b834bb74b8fdbdcf5b0d1c535f54ce71
Slavcho Mitrov <mitrov.slavcho@gmail.com>
2023-11-05 14:16:18 +0100

    improve the drag transition and create drag handle button and component

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -1,9 +1,10 @@
 <template>
     <div>
-        <draggableComponent v-model="columns" group="columns" item-key="id" class="flex gap-4 overflow-x-auto items-start">
+        <draggableComponent v-model="columns" group="columns" :animation="150" handle=".drag-handle" item-key="id" class="flex gap-4 overflow-x-auto items-start">
             <template #item="{ element: column }: { element: Column }">
                 <div class="column bg-gray-200 p-5 rounded min-w-[250px]">
                     <header class="font-bold mb-4">
+                        <DragHandle/>
                         {{ column.title }}
                     </header>
                     <TrelloBoardTask v-for="task in column.tasks" :key="task.id" :task="task" />
