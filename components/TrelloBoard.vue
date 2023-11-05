e450d0d04b999dd056fd2bd8d4df8c68d4aa6e8e
Slavcho Mitrov <mitrov.slavcho@gmail.com>
2023-11-05 16:43:53 +0100

    enable deletion of tasks with backspace

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -12,7 +12,7 @@
                         handle=".drag-handle" :animation="150" item-key="id">
                         <template #item="{ element: task }: { element: Task }">
                             <div>
-                                <TrelloBoardTask :task="task" />
+                                <TrelloBoardTask :task="task" @delete="column.tasks = column.tasks.filter(t => t.id !== $event)" />
                             </div>
                         </template>
                     </draggableComponent>
